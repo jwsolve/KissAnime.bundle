993c09b1db9a5f4a6cffaf52a4264a6ed254b9be
jwsolve <jwsolve@gmail.com>
2015-03-26 22:55:48 +0000

    Updated HTTP Headers

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -21,6 +21,10 @@ def Start():
 	ObjectContainer.title1 = TITLE
 	DirectoryObject.thumb = R(ICON_SERIES)
 	VideoClipObject.thumb = R(ICON_SERIES)
+
+	HTTP.CacheTime = CACHE_1HOUR
+	HTTP.Headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36"
+	HTTP.Headers['Host'] = "kissanime.com"
 	
 ######################################################################################
 # Menu hierarchy
