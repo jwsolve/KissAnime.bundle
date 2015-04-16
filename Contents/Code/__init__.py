775b335c0ab26b1264e2c04365fd4585fa1f6f4c
jwsolve <jwsolve@gmail.com>
2015-04-16 20:35:24 +0100

    Added cloudflare protection bypass

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -13,6 +13,21 @@ ART = "art-default.jpg"
 BASE_URL = "http://kissanime.com"
 SEARCH_URL = "http://kissanime.com/Search/Anime"
 
+import os
+import sys
+from lxml import html
+
+try:
+	path = os.getcwd().split("?\\")[1].split('Plug-in Support')[0]+"Plug-ins/KissAnime.bundle/Contents/Code/Modules/KissAnime"
+except:
+	path = os.getcwd().split("Plug-in Support")[0]+"Plug-ins/KissAnime.bundle/Contents/Code/Modules/KissAnime"
+if path not in sys.path:
+	sys.path.append(path)
+
+import cfscrape
+scraper = cfscrape.create_scraper()
+myrequest = scraper.get(BASE_URL + '/AnimeList')
+
 ######################################################################################
 # Set global variables
 
@@ -25,14 +40,27 @@ def Start():
 	HTTP.CacheTime = CACHE_1HOUR
 	HTTP.Headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36"
 	HTTP.Headers['Host'] = "kissanime.com"
-	
+
 ######################################################################################
 # Menu hierarchy
 
 @handler(PREFIX, TITLE, art=ART, thumb=ICON)
 def MainMenu():
 
-	return Shows()
+	oc = ObjectContainer()
+	oc.add(InputDirectoryObject(key = Callback(Search), title='Search', summary='Search Kisscartoon', prompt='Search for...'))
+	pagehtml = html.fromstring(myrequest.text)
+	#html = HTML.ElementFromURL(BASE_URL + '/AnimeList')
+	for each in pagehtml.xpath("//div[@class='alphabet']/a"):
+		title = each.xpath("./text()")[0]
+		url = each.xpath("./@href")[0]
+		oc.add(DirectoryObject(
+			key = Callback(ShowCartoons, title = title, url = url, page_count = 1),
+				title = title,
+				thumb = ICON_SERIES
+				)
+		)
+	return oc
 
 ######################################################################################
 # Creates page url from category and creates objects from that page
@@ -42,9 +70,10 @@ def Shows():
 
 	oc = ObjectContainer()
 	oc.add(InputDirectoryObject(key = Callback(Search), title='Search', summary='Search Kisscartoon', prompt='Search for...'))
-	html = HTML.ElementFromURL(BASE_URL + '/AnimeList')
+	pagehtml = html.fromstring(myrequest.text)
+	#html = myrequest.ElementFromURL(BASE_URL + '/AnimeList', headers=HTTP_HEADERS)
 
-	for each in html.xpath("//div[@class='alphabet']/a"):
+	for each in page.xpath("//div[@class='alphabet']/a"):
 		title = each.xpath("./text()")[0]
 		url = each.xpath("./@href")[0]
 		oc.add(DirectoryObject(
@@ -61,9 +90,10 @@ def ShowCartoons(title, url, page_count):
 	oc = ObjectContainer(title1 = title)
 	thisurl = url
 	thisletter = url.split("=",1)[1]
-	html = HTML.ElementFromURL(BASE_URL + '/AnimeList' + url + '&page=' + page_count)
+	page = scraper.get(BASE_URL + '/AnimeList' + url + '&page=' + page_count)
+	pagehtml = html.fromstring(page.text)
 
-	for each in html.xpath("//tr/td[1]"):
+	for each in pagehtml.xpath("//tr/td[1]"):
 		url = each.xpath("./a/@href")[0]
 		thumb = ""
 		title = each.xpath("./a/text()")[0]
@@ -86,8 +116,10 @@ def ShowCartoons(title, url, page_count):
 def ShowEpisodes(title, url):
 
 	oc = ObjectContainer(title1 = title)
-	html = HTML.ElementFromURL(BASE_URL + url)
-	for each in html.xpath("//table[@class='listing']/tr/td[1]"):
+	page = scraper.get(BASE_URL + url)
+	pagehtml = html.fromstring(page.text)
+
+	for each in pagehtml.xpath("//table[@class='listing']/tr/td[1]"):
 		url = each.xpath("./a/@href")[0]
 		title = each.xpath("./a/text()")[0]
 		thumb = ""
@@ -104,10 +136,11 @@ def ShowEpisodes(title, url):
 def EpisodeDetail(title, url):
 	
 	oc = ObjectContainer(title1 = title)
-	page = HTML.ElementFromURL(BASE_URL + url)
-	title = page.xpath("//option[@selected='selected']/text()")[0]
-	description = page.xpath("//meta[@name='description']/@content")[0]
-	thumb = page.xpath("//meta[@property='og:image']/@content")[0]
+	page = scraper.get(BASE_URL + url)
+	pagehtml = html.fromstring(page.text)
+	title = pagehtml.xpath("//option[@selected='selected']/text()")[0]
+	description = pagehtml.xpath("//meta[@name='description']/@content")[0]
+	thumb = pagehtml.xpath("//meta[@property='og:image']/@content")[0]
 	
 	oc.add(VideoClipObject(
 		title = title,
