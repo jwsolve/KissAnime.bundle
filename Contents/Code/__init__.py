d78dbc010c7f59a80f49d39ebc2cbd53cd40e472
jwsolve <jwsolve@gmail.com>
2015-11-23 00:28:01 +0000

    Updated Next Button for Latest and Most Popular

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -9,6 +9,7 @@ PREFIX = "/video/kissanime"
 ICON = "icon-default.png"
 ICON_SERIES = "icon-default.png"
 ICON_NEXT = "icon-next.png"
+ICON_QUEUE = "icon-cover.png"
 ART = "art-default.jpg"
 BASE_URL = "http://kissanime.com"
 SEARCH_URL = "http://kissanime.com/Search/Anime"
@@ -16,6 +17,7 @@ SEARCH_URL = "http://kissanime.com/Search/Anime"
 import os
 import sys
 from lxml import html
+import base64
 
 try:
 	path = os.getcwd().split("?\\")[1].split('Plug-in Support')[0]+"Plug-ins/KissAnime.bundle/Contents/Code/Modules/KissAnime"
@@ -24,9 +26,12 @@ except:
 if path not in sys.path:
 	sys.path.append(path)
 
+import requests
 import cfscrape
 scraper = cfscrape.create_scraper()
 myrequest = scraper.get(BASE_URL + '/AnimeList')
+username = Prefs['username']
+password = Prefs['password']
 
 ######################################################################################
 # Set global variables
@@ -39,20 +44,20 @@ def Start():
 
 	HTTP.CacheTime = CACHE_1HOUR
 	HTTP.Headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36"
-	HTTP.Headers['Host'] = "kissanime.com"
 
 ######################################################################################
 # Menu hierarchy
 
 @handler(PREFIX, TITLE, art=ART, thumb=ICON)
 def MainMenu():
-
 	oc = ObjectContainer()
+	oc.add(PrefsObject(title=L('preferences')))
 	oc.add(InputDirectoryObject(key = Callback(Search), title='Search', summary='Search Kisscartoon', prompt='Search for...'))
 	oc.add(DirectoryObject(key = Callback(AToZ), title = "A to Z"))
-	oc.add(DirectoryObject(key = Callback(LatestEpisodes, title="Latest Updates", url = '/LatestUpdate/', page_count = 1), title = "Latest Updates"))
-	oc.add(DirectoryObject(key = Callback(LatestEpisodes, title="Most Popular", url = '/MostPopular/', page_count = 1), title = "Most Popular"))
-	oc.add(DirectoryObject(key = Callback(LatestEpisodes, title="New Anime", url = '/Newest/', page_count = 1), title = "New Anime"))
+	oc.add(DirectoryObject(key = Callback(LatestEpisodes, title="Latest Updates", url = '/LatestUpdate', page_count = 1), title = "Latest Updates"))
+	oc.add(DirectoryObject(key = Callback(LatestEpisodes, title="Most Popular", url = '/MostPopular', page_count = 1), title = "Most Popular"))
+	oc.add(DirectoryObject(key = Callback(LatestEpisodes, title="New Anime", url = '/Newest', page_count = 1), title = "New Anime"))
+	oc.add(DirectoryObject(key = Callback(Bookmarks, title="My Bookmarks"), title = "My Bookmarks", thumb = R(ICON_QUEUE)))
 	return oc
 
 ######################################################################################
@@ -66,12 +71,13 @@ def AToZ():
 	for each in pagehtml.xpath("//div[@class='alphabet']/a"):
 		title = each.xpath("./text()")[0]
 		url = each.xpath("./@href")[0]
-		oc.add(DirectoryObject(
-			key = Callback(ShowCartoons, title = title, url = url, page_count = 1),
-				title = title,
-				thumb = ICON_SERIES
-				)
-		)
+		if title != "All":
+			oc.add(DirectoryObject(
+				key = Callback(ShowCartoons, title = title, url = url, page_count = 1),
+					title = title,
+					thumb = ICON_SERIES
+					)
+			)
 	return oc
 
 ######################################################################################
@@ -116,7 +122,7 @@ def LatestEpisodes(title, url, page_count):
 				)
 		)
 	oc.add(NextPageObject(
-		key = Callback(ShowCartoons, url = thisurl, title = thistitle, page_count = int(page_count) + 1),
+		key = Callback(LatestEpisodes, url = thisurl, title = thistitle, page_count = int(page_count) + 1),
 		title = "More...",
 		thumb = R(ICON_NEXT)
 			)
@@ -138,10 +144,7 @@ def ShowCartoons(title, url, page_count):
 		thumbhtml = scraper.get(BASE_URL + url)
 		page_html = html.fromstring(thumbhtml.text)
 		title = page_html.xpath("//a[@class='bigChar']/text()")[0]
-		try:
-			thumb = page_html.xpath("//div[@class='barContent']/div/img/@src")[0]
-		except:
-			thumb = ICON_SERIES
+		thumb = page_html.xpath("//link/@href")[0]
 		oc.add(DirectoryObject(
 			key = Callback(ShowEpisodes, title = title, url = url),
 				title = title,
@@ -222,3 +225,43 @@ def Search(query):
 				)
 		)
 	return oc
+
+######################################################################################
+# Loads bookmarked shows from Dict.  Titles are used as keys to store the show urls.
+
+@route(PREFIX + "/bookmarks")	
+def Bookmarks(title):
+
+	oc = ObjectContainer(title1 = title)
+	post_values = {
+		'username' : username,
+		'password' : password
+		}
+
+	if username and password:
+		sess = requests.session()
+		s = cfscrape.create_scraper(sess)
+		page = s.post("http://kissanime.com/Login", post_values)
+		#bookmarks = s.get(BASE_URL + '/BookmarkList')
+		#pagehtml = html.fromstring(bookmarks.text)
+		return MessageContainer(
+			"Success",
+			page.text
+		)
+		for each in pagehtml.xpath("//a[@class='aAnime']"):
+			url = each.xpath("./@href")[0]
+			title = each.xpath("./text()")[0]
+			thumb = ""
+		
+			oc.add(DirectoryObject(
+				key = Callback(EpisodeDetail, title = title, url = url),
+				title = title,
+				thumb = Resource.ContentsOfURLWithFallback(url = thumb, fallback='icon-cover.png')
+				)
+			)
+		return oc
+	else:
+		return MessageContainer(
+			"Error",
+			"You need to provide a username and password"
+		)
